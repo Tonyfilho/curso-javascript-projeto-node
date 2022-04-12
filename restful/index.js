2a14c46c6899037b1494aa1d4dcb36404fe483e8
unknown <oficinadosdrones@gmail.com>
2022-04-12 17:22:36 +0100

    usando o express

--- restful/index.js
+++ restful/index.js
@@ -1,42 +1,31 @@
-const http= require('http'); //carregando modulo http
+const express= require('express'); //carregando modulo http
+
+/**Cria a var APP e o express ja traz o metodo GET */
+let app = express();
+/**Aqui no express, temos como paramentro a rota, NESTE CASO O barra */
+app.get('/',(req, res) => {
+    /** A Resposta é o que colocarmos aqui em baixo, feito isto fecha a rota e cria outra */
+    res.statusCode = 200; //
+    res.setHeader('Content-Type', 'text/html');
+    res.end('<h1>Server rodando</h1>');
+});
 
 
-/**Criando o servidor com a função server, guardamos este servidor dentro da var SERVER */
-let server = http.createServer((req, res) => {
-
-    console.log('URL:', req.url); //aqui mostra qual URL chamada no servidor
-    console.log('METHOD:', req.method); //aqui mostra qual o Metodo chamado no Servidor
-
- //   res.end('OK');//aqui é a resposta para o servidor ficar rodando sem parar
-    switch (req.url) {
-        case '/':
-            res.statusCode = 200; //
-            res.setHeader('Content-Type', 'text/html');
-            res.end('<h1>Server rodando</h1>');
-            break;
-    
-        case '/users':
-            res.statusCode = 200; //
-            res.setHeader('Content-Type', 'application/json');
-            res.end(JSON.stringify({
-                users: [{
-                    name: 'tony',
-                    email: 'tony@gmail.com',
-                    id: 1
-                }]
-            }));
-            break;
-    
-        default:
-            res.statusCode = 404
-            res.setHeader('Content-Type', 'text/html');
-            res.end('<h1>Http not found</h1>');
-            break;
-    }
+app.get('/users',(req, res) => {
+    res.statusCode = 200; //
+    res.setHeader('Content-Type', 'application/json');
+    res.json({
+        users: [{
+            name: 'tony',
+            email: 'tony@gmail.com',
+            id: 1
+        }]
+    });
 
 });
+
 /**Pela Variavel q foi atribuida, chamamos o metodo Listen para ficar ouvindo na porta 3000 */
-server.listen(3000, '127.0.0.1', () => {
+app.listen(3000, '127.0.0.1', () => {
 
     console.log("servidor rodando!");
 
