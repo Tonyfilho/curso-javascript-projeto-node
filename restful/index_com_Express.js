18047ae09a9d33a4ddc99b4f6c680aedab4f2498
TonyFilho <oficinadosdrones@gmail.com>
2022-04-13 10:43:59 +0100

    acertando os novos nomes

---
+++ restful/index_com_Express.js
@@ -0,0 +1,20 @@
+const express= require('express'); //carregando modulo http
+const routerIndex = require('./routes/index');
+const routerUsers = require('./routes/users');
+
+/**Cria a var APP e o express ja traz o metodo GET */
+let app = express();
+
+/** Depois de feitas as Rotas, aqui no App, passamos a variaveis */
+app.use('/', routerIndex);
+app.use('/users', routerUsers);
+
+
+
+
+/**Pela Variavel q foi atribuida, chamamos o metodo Listen para ficar ouvindo na porta 3000 */
+app.listen(3000, '127.0.0.1', () => {
+
+    console.log("servidor rodando!");
+
+});
