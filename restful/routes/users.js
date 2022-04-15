1a8d89651da8190286504d53740cc62d8f233941
unknown <oficinadosdrones@gmail.com>
2022-04-15 08:58:44 +0100

    fazendo o findOne()

--- restful/routes/users.js
+++ restful/routes/users.js
@@ -11,7 +11,7 @@ let db = new NeDB({
 
 /** aqui agora passamos a Var APP como paramentro de uma função do CONSIGN com o nome da todas as rotas*/
 module.exports = (app) => {
-  const routes = '/users';
+  const routes = "/users";
   /**Find é o GetAll, sem paramentros ele traz tudo, caso queira algum item passe o Objeto desejado
    * 1º Sort orderna o resultado  1 é ASC e -1 é DESC, neste caso pela Coluna NOME:1
    * 2º Exec, onde temos 2 paramentros, err e success
@@ -21,9 +21,9 @@ module.exports = (app) => {
       .sort({ name: 1 })
       .exec((err, success) => {
         if (err) {
-        app.utils.errors.send(err, req, res, 400, 'Error no Find All');
-        } else {       
-          res.json({users: success});
+          app.utils.errors.send(err, req, res, 400, "Error no Find All");
+        } else {
+          res.json({ users: success });
           res.status(200).json;
         }
       });
@@ -45,25 +45,48 @@ module.exports = (app) => {
         // res.status(400).json({
         //   error: err,
         // }); //um status code do error
-        app.utils.errors.send(err, req, res, 400, 'Error Post');
+        app.utils.errors.send(err, req, res, 400, "Error Post");
       } else {
         /** e mandamos um JSON com o success que foi salvo na DB */
         res.status(200).json(success);
       }
     });
   });
-
-  app.get(`${routes}/admin`, (req, res) => {
-    res.statusCode = 200; //
-    res.setHeader("Content-Type", "application/json");
-    res.json({
-      admins: [
-        {
-          name: "tony",
-          email: "tony@gmail.com",
-          id: 1,
-        },
-      ],
+  /**Buscando somente 1 usuário pela id,
+   * NOTE: Usamos uma VAR ROUTEID para ja receber de APP a rota,
+   *  e com isto não precisamos escrever lá no GET
+   * 1º Usaremos o FindOne();
+   * 2º Usaremos o atributo _id e o paramentro que o _id:req.params.id vai receber
+   * 3º Oreq.params.id . o Final ID tem q ser o mesmo que é passado no app.route('/users/:id')
+   */
+  let routeId = app.route("/users/:id");
+  routeId.get((req, res) => {
+    db.findOne({ _id: req.params.id }).exec((err, success) => {
+      if (err) {
+        app.utils.errors.send(
+          err,
+          req,
+          res,
+          400,
+          "Erro ao Buscar o usuário pela Id"
+        );
+      } else {
+        res.status(200).json(success);
+      }
     });
   });
-};
+
+  // app.get(`${routes}/admin`, (req, res) => {
+  //   res.statusCode = 200; //
+  //   res.setHeader("Content-Type", "application/json");
+  //   res.json({
+  //     admins: [
+  //       {
+  //         name: "tony",
+  //         email: "tony@gmail.com",
+  //         id: 1,
+  //       },
+  //     ],
+  //   });
+  // });
+}; //end class
